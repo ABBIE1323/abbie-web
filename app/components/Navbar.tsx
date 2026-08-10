997927d25378763fa7a8fb934d10096a885f02d0
ABBIE1323 <6821901025@cdti.ac.th>
2026-08-10 10:25:14 +0700

    update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -74,7 +74,7 @@ export default function Navbar() {
     <nav className="navbar">
       <div className="container">
         <Link href="/" className="logo">
-          ROBOT SHOP
+          Punnapat Nuansrichok
         </Link>
 
         <button className="menu-btn" onClick={() => setMenuOpen(!menuOpen)}>
